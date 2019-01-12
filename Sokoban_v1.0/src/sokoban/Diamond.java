650884dd9893b889d0c581540e141dbb22139c59
Ma-Nic <43272457+Ma-Nic@users.noreply.github.com>
2019-01-12 01:44:42 +0000

    Add files via upload

---
+++ Sokoban_v1.0/src/sokoban/Diamond.java
@@ -0,0 +1,51 @@
+/* Matt Nicol - 09001885
+ * UINH17135 - OOP Sokoban v1.0
+ * 11/01/19
+ * Eclipse V2018-09 4.9.0 
+ */
+
+
+package sokoban;
+
+
+
+public class Diamond extends MapElement {
+	
+	private boolean hasCrate = false;
+	
+	public Diamond()
+	{
+		
+	}
+
+	// Construct diamond object and set x and y coordinates
+	public Diamond(int x, int y)
+	{
+		this.setX(x);
+		this.setY(y);
+	}
+	
+	/**
+	 * @param dhc Boolean value to set whether the diamond has a crate or not
+	 */
+	public void setHasCrate(boolean dhc) 
+	{
+			hasCrate = dhc;
+	}
+    
+	/**
+	 * @return Return whether diamond has a crate or not
+	 */
+	public boolean getHasCrate()
+	{
+		return hasCrate;
+	}
+	
+	@Override
+	public String filePath()
+	{
+		String fn = "res/Diamond.png";
+		return fn;
+	}
+
+}	//END of Diamond
---
+++ Sokoban_v1.0/src/sokoban/JUnitTest.java
@@ -0,0 +1,35 @@
+/* Matt Nicol - 09001885
+ * UINH17135 - OOP Sokoban v1.0
+ * 11/01/19
+ * Eclipse V2018-09 4.9.0 
+ */
+
+
+package sokoban;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class JUnitTest {
+
+
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	
+	@Test
+	void testLevelSelectFail() 
+	{
+	
+	}
+	
+	
+}
