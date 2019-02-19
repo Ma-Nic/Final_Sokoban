3a25fb96e7ff561bb57d7dcfb76a4fb5c0ff8f85
Ma-Nic <43272457+Ma-Nic@users.noreply.github.com>
2019-02-19 23:56:07 +0000

    Add files via upload

---
+++ src/sokoban/Crate.java
@@ -0,0 +1,105 @@
+/* Matt Nicol - 09001885
+ * UINH17135 - OOP Sokoban v2.0
+ * 19/02/19
+ * Eclipse V2018-09 4.9.0 
+ */
+
+
+package sokoban;
+
+
+
+public class Crate extends MapElement implements MovableMapElmt {
+
+	private String fn = "res/Crate.png";
+	
+	public Crate()
+	{
+		
+	}
+	
+	// Construct crate object and set x and y coordinates
+	public Crate(int x, int y)
+	{
+		this.setX(x);
+		this.setY(y);
+	}
+	
+	// Check crate on diamond status, if true return Crate in Place image
+	public void crateStatus(boolean cStatus)
+	{
+		if(cStatus == true)
+		{
+			this.fn = "res/CrateInPlace.png";
+		}
+		else this.fn = "res/Crate.png";
+	}
+	
+	@Override
+	public String filePath()
+	{
+		return fn;
+	}
+	
+	// Update location of crate if moved
+	@Override
+	public void moveLocation(int x, int y) {
+        int newX = getX() + x;
+        int newY = getY() + y;
+        setX(newX);
+        setY(newY);
+	}
+
+	/**
+	 * Checks up movement (y - 1) for collisions against other elements
+	 * @return true if collision is detected 
+	 */
+	@Override
+	public boolean collisionUp(MapElement checkColl) {
+		if(getY()-1 == checkColl.getY() && getX() == checkColl.getX()) 
+		{
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Checks down movement (y + 1) for collisions against other elements
+	 * @return true if collision is detected 
+	 */
+	@Override
+	public boolean collisionDown(MapElement checkColl) {
+		if(getY()+1 == checkColl.getY() && getX() == checkColl.getX()) 
+		{
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Checks left movement (x - 1) for collisions against other elements
+	 * @return true if collision is detected 
+	 */
+	@Override
+	public boolean collisionLeft(MapElement checkColl) {
+		if(getX()-1 == checkColl.getX() && getY() == checkColl.getY()) 
+		{
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Checks right movement (x + 1) for collisions against other elements
+	 * @return true if collision is detected 
+	 */
+	@Override
+	public boolean collisionRight(MapElement checkColl) {
+		if(getX()+1 == checkColl.getX() && getY() == checkColl.getY()) 
+		{
+			return true;
+		}
+		return false;
+	}
+	
+}	//END of Crate
---
+++ src/sokoban/MapElement.java
@@ -0,0 +1,44 @@
+/* Matt Nicol - 09001885
+ * UINH17135 - OOP Sokoban v2.0
+ * 19/02/19
+ * Eclipse V2018-09 4.9.0 
+ */
+
+
+package sokoban;
+
+public abstract class MapElement {			// Super class for all map elements, cannot be instantiated itself
+
+	private int x_value = 0;
+	private int y_value = 0;
+	
+	//Get x coord
+	public int getX() 
+	{
+		return x_value;
+	}
+	
+	//Get y coord
+	public int getY() 
+	{
+		return y_value;
+	}
+	
+	//Set x coord
+	public void setX(int newX)
+	{
+		x_value = newX;
+	}
+	
+	//Set y coord
+	public void setY(int newY)
+	{
+		y_value = newY;
+	}
+	
+	public String filePath() 
+	{
+		return null;
+	}
+	
+}	// END MapElement
