6ed3b1e77c2635eda7dc4c05bad0f7a47e2f0a97
Ma-Nic <43272457+Ma-Nic@users.noreply.github.com>
2019-02-20 00:04:10 +0000

    Add files via upload

---
+++ Sokoban_v2.0/src/sokoban/JUnitTest.java
@@ -0,0 +1,61 @@
+/* Matt Nicol - 09001885
+ * UINH17135 - OOP Sokoban v2.0
+ * 19/02/19
+ * Eclipse V2018-09 4.9.0 
+ */
+
+
+package sokoban;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class JUnitTest {
+
+
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	
+	@Test
+	void testGetX() {
+		Crate x = new Crate();
+		x.setX(12);
+		int answer = x.getX();
+		assertEquals(12, answer);
+	}
+	
+	@Test
+	void testGetY() {
+		Crate y = new Crate();
+		y.setY(6);
+		int answer = y.getY();
+		assertEquals(6, answer);
+	}
+	
+	@Test
+	void testCrateOnDiamond() {
+		Diamond d = new Diamond();
+		d.setHasCrate(true);
+		boolean answer = d.getHasCrate();
+		assertEquals(true, answer);
+	}
+	
+	@Test
+	void testNoCrateOnDiamond() {
+		Diamond d = new Diamond();
+		d.setHasCrate(false);
+		boolean answer = d.getHasCrate();
+		assertEquals(false, answer);
+	}
+	
+	
+}
---
+++ Sokoban_v2.0/src/sokoban/MovableMapElmt.java
@@ -0,0 +1,29 @@
+/* Matt Nicol - 09001885
+ * UINH17135 - OOP Sokoban v2.0
+ * 19/02/19
+ * Eclipse V2018-09 4.9.0 
+ */
+
+
+package sokoban;
+
+public abstract interface MovableMapElmt {			// Interface for movable map element required methods
+
+	// Sets new coordinates for movable objects
+	void moveLocation(int x, int y);
+	
+    // Check for collisions when moving up
+    boolean collisionUp(MapElement checkColl);
+  
+    // Check for collisions when moving down
+    boolean collisionDown(MapElement checkColl);
+    
+    // Check for collisions when moving left
+    boolean collisionLeft(MapElement checkColl);
+
+    // Check for collisions when moving right
+    boolean collisionRight(MapElement checkColl);
+   
+}	//END MovableMapElmt
+	
+
